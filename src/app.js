25dcc9012a18bec136eb6e2a1f6698e466d834a3
Oleksii Habiiev <oleksii.habiiev@gmail.com>
2019-09-22 18:40:44 +0300

    Add morgan support

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+const config = require('./config');
 const router = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
+
+if (config.env === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(router);
 
