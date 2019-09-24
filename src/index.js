34c98f8eeaeee11cd4b29c8f2f18fd051e832f6f
Oleksii Habiiev <oleksii.habiiev@gmail.com>
2019-09-24 09:54:55 +0300

    Remove mongoose deprecation warnings. Start the server only after database connection was established

--- src/index.js
+++ src/index.js
@@ -7,18 +7,17 @@ require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const config = require('./config');
 
-mongoose
-  .connect(config.dbUrl)
-  .then(() => console.log('Connected to Mongo'))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
-
 const app = require('./app');
 
-const server = http
-  .createServer(app)
-  .listen(config.port, config.host, () =>
-    console.log(`Server on ${server.address().address}:${server.address().port}`)
-  );
+const server = http.createServer(app);
+
+mongoose
+  .connect(config.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    server.listen(config.port, config.host, () =>
+      console.log(`Server on ${server.address().address}:${server.address().port}`)
+    )
+  )
+  .catch(err => {
+    console.error(err);
+  });
