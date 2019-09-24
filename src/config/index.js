5a29c3d0e6e95e08a63f794bdc8a126b1ed574fb
Oleksii Habiiev <oleksii.habiiev@gmail.com>
2019-09-24 09:51:24 +0300

    Install mongoose and add a connection to the database

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   port: process.env.PORT || 5500,
-  host: process.env.HOST || '127.0.0.1'
+  host: process.env.HOST || '127.0.0.1',
+  dbUrl: process.env.MONGO || 'mongodb://localhost:27017/blog'
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const http = require('http');
 const path = require('path');
 
+const mongoose = require('mongoose');
+
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
-const app = require('./app');
 const config = require('./config');
+
+mongoose
+  .connect(config.dbUrl)
+  .then(() => console.log('Connected to Mongo'))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+const app = require('./app');
 
 const server = http
   .createServer(app)
