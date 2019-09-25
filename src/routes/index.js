b5e50ce3520351cc7956606558acf5d1f93072eb
Oleksii Habiiev <oleksii.habiiev@gmail.com>
2019-09-25 22:01:33 +0300

    Added basic error handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
-router.get('/', require('./articles'));
+router.use('/', require('./articles'));
+
+router.get('/error', (req, res, next) => next(Error('Test error')));
+
+router.all('*', (req, res) => res.status(404).render('notFound', { title: 'Page Not Found' }));
+
+router.use((err, req, res, next) =>
+  res.status(err.status || 500).render('error', { status: err.status || 500, message: err.message })
+);
 
 module.exports = router;
