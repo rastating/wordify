740798910160bc93b894838b06435a9796ab07a7
Oleksii Habiiev <oleksii.habiiev@gmail.com>
2019-10-04 00:33:06 +0300

    Fix a bug when preloading article function would not exit the function if the article is not found

--- src/controllers/articles.controller.js
+++ src/controllers/articles.controller.js
@@ -10,10 +10,11 @@ exports.preLoadArticle = (req, res, next, slug) => {
   Article.findOne({ slug })
     .populate('author')
     .then(article => {
+      console.log(!article);
       if (!article) {
         const error = new Error('Article was not found');
         error.status = 404;
-        next(error);
+        return next(error);
       }
 
       req.article = article;
